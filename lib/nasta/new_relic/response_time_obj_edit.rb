c54411dc0a4269b6cc11324043abdf4878bb2206
Samuel Szabo <samuelszabo7+git@gmail.com>
2020-03-11 13:19:12 +0100

    Report: ResponseTimeObjEdit

---
+++ lib/nasta/new_relic/response_time_obj_edit.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+require 'net/http'
+require 'json'
+require 'time'
+require 'nasta/service/new_relic_insights'
+
+module NewRelic
+  class ResponseTimeObjEdit
+    def fetch
+      api = NewRelicInsights.new
+      data = api.call 'SELECT percentile(duration, 99) FROM Transaction SINCE 5 minutes AGO WHERE name LIKE \'%obj_edit%\' AND `request.method` = \'GET\''
+      [['response_time_obj_edit', Time.now.to_i, data[0]['percentiles']['99']]]
+    end
+  end
+end
